f5be6e15d873b292946f63d2fe3f0ac9b839f317
eclipse5685 <eclipse5685@gmail.com>
2020-08-19 03:15:19 +0000

    階乗関数に変更

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 'use strict';
 
-const number = process.argv[2] || 0;
-let sum = 0;
+/**
+ * 与えられた自然数の階乗を返す
+ * 階乗とは、1からその与えられた自然数までの数をかけたものです
+ * @param {numver} n
+ * @returs {Number}
+ */
 
-for( let i = 0; i <= number; i++ )
-{
-    sum = sum * i;
-}
-console.log(sum);
+ function factorial(n)
+ {
+     let result = 1;
+     for( let i = 1; i <= n ; i++)
+     {
+         result = result * i;
+     }
+     return result;
+ }
+
+ const assert = require('assert');
+
+ assert.equal(factorial(1),1, '1の階乗は1ですが、実際は${factorial(1)}でした');
+ assert.equal(factorial(2),2, '2の階乗は2ですが、実際は${factorial(2)}でした');
+ assert.equal(factorial(3),6, '3の階乗は6ですが、実際は${factorial(3)}でした');
+ assert.equal(factorial(10),3628800,'10の階乗は3628800ですが、実際は${factorial(10)}でした');
+console.log('すべてのテストを通過した');
+
